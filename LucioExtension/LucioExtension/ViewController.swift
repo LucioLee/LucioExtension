2f74e44c07aa326bd27078b4fbd8f4af2ce11c0e
LucioLee <990467229@qq.com>
2016-03-10 15:11:14 +0800

    init

---
+++ LucioExtension/LucioExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LucioExtension
+//
+//  Created by Lucio on 16/3/10.
+//  Copyright © 2016年 Person. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
